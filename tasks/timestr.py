2d19b54319588227e325d103710452af4e4a2978
Beatrice Mitskevich <105979643+beatrice-portinari@users.noreply.github.com>
2023-02-06 20:13:14 +0300

    Update timestr.py

--- tasks/timestr.py
+++ tasks/timestr.py
@@ -3,16 +3,21 @@ __all__ = (
 )
 
 
-def seconds_to_str(seconds: int) -> str:
-    """
-    Функция должна вернуть текстовое представление времени
-    20 -> 20s
-    60 -> 01m00s
-    65 -> 01m05s
-    3700 -> 01h01m40s
-    93600 -> 01d02h00m00s
-    """
-    raise NotImplementedError
+def seconds_to_str(seconds):
+    time_parameters = {
+        "d": 3600 * 24,
+        "h": 60 * 60,
+        "m": 60,
+    }
+    result = ""
+    for key in time_parameters.keys():
+        if num := (seconds // time_parameters[key]):
+            result += f"{num:0>2}{key}"
+        elif result:
+            result += f"00{key}"
+        seconds -= num * time_parameters[key]
+    result += f"{seconds:0>2}s"
+    return result
 
 
 
