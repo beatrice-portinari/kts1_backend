672cc53cfc0f4faac4d325da7b01ba0d638e76f0
Beatrice Mitskevich <105979643+beatrice-portinari@users.noreply.github.com>
2023-02-06 20:09:11 +0300

    Update even_odd.py

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -3,10 +3,11 @@ __all__ = (
 )
 
 
-def even_odd(arr: list[int]) -> float:
-    """
-    Функция возвращает отношение суммы четных элементов массив к сумме нечетных
-    Пример:
-    even_odd([1, 2, 3, 4, 5]) == 0.8889
-    """
-    raise NotImplementedError
+def even_odd(lst):
+    even = [i for i in lst if i % 2 == 0]
+    odd = list(set(lst) - set(even))
+    try:
+        result = round(sum(even) / sum(odd), 5)
+    except ZeroDivisionError:
+        return 0
+    return result
