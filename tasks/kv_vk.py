deb95129905284371c0fa0894c83dead4fb243b1
Beatrice Mitskevich <105979643+beatrice-portinari@users.noreply.github.com>
2023-02-06 20:11:19 +0300

    Update kv_vk.py

--- tasks/kv_vk.py
+++ tasks/kv_vk.py
@@ -10,33 +10,11 @@ KT = TypeVar('KT')
 KV = TypeVar('KV')
 
 
-def flip_kv_vk(d: dict[KT, KV]) -> dict[KV, KT]:
-    """
-    Функция должна возвращать словарь, в котором в качестве ключей - значения
-    переданного словаря, а в качестве значений - ключи.
-    Например,
-    flip_kv_vk({
-        'tokyo': 'Токио',
-        'moscow': 'Москва',
-    }) == {
-        'Токио': 'tokyo',
-        'Москва': 'moscow',
-    }
-    """
-    raise NotImplementedError
+def flip_kv_vk(data):
+    return {value: key for key, value in data.items()}
 
-
-def flip_kv_vk_safe(d: dict[KT, KV]) -> dict[KV, list[KT]]:
-    """
-    Функция должна возвращать словарь, в котором в качестве ключей - значения
-    переданного словаря, а в качестве значений - массив ключей конфликтующих
-    значений.
-    Например,
-    flip_kv_vk({
-        'Санкт-Петербург': '+3',
-        'Москва': '+3',
-    }) == {
-        '+3': ['Москва', 'Санкт-Петербург'],
-    }
-    """
-    raise NotImplementedError
+def flip_kv_vk_safe(data):
+    result = {}
+    for key, value in data.items():
+        result.setdefault(value, []).append(key)
+    return result
