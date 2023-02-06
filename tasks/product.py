8b5a98446161fe08ffd0af79982fb33850b0d584
Beatrice Mitskevich <105979643+beatrice-portinari@users.noreply.github.com>
2023-02-06 20:15:07 +0300

    Update product.py

--- tasks/product.py
+++ tasks/product.py
@@ -5,9 +5,10 @@ __all__ = (
 )
 
 
-def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
-    """
-    Должна возвращать все пары элементы двух массивов:
-    cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
-    """
-    raise NotImplementedError
+import itertools
+
+def cartesian_product(lst1, lst2):
+    result = []
+    for item in itertools.product(*[lst1, lst2]):
+        result.append(item)
+    return result
