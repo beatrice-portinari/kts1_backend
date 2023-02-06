d2574e857de56cdb82aad89dca24f10fdb8c42ea
user <mitckevich.123@yandex.ru>
2023-02-06 20:32:58 +0300

    Changes to be committed: 	modified:   graph.py

--- tasks/graph.py
+++ tasks/graph.py
@@ -8,6 +8,23 @@ __all__ = (
 
 from collections import deque
 
+class Node:
+    def __init__(self, value: Any):
+        self.value = value
+
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other: 'Node'):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f'Node({repr(self.value)})'
+
+    __repr__ = __str__
+
+
 class Graph:
     def __init__(self, root: Node):
         self._root = root
